602da3e4830acd561bd43da3f0b92ced010c2728
gacdutra <ga.acdutra@gmail.com>
2022-10-12 16:44:11 -0300

    feat: add database.js connected to local database

---
+++ src/database/database.js
@@ -0,0 +1,23 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+// const connectionString = process.env.DATABASE_URL;
+
+const user = process.env.LOCAL_DATABASE_USER;
+const password = process.env.LOCAL_DATABASE_PASSWORD;
+const host = "localhost";
+const port = 5432;
+const database = "projeto16-shortly";
+
+const connection = new Pool({
+  user,
+  password,
+  host,
+  port,
+  database,
+});
+
+export default connection;
