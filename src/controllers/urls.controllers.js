8c31a93dd47f74b01c65259b9546c7db6c328651
gacdutra <ga.acdutra@gmail.com>
2022-10-13 12:25:25 -0300

    feat: add GET/urls/:id

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -27,4 +27,32 @@ async function shortUrl(req, res) {
   }
 }
 
-export { shortUrl };
+async function getUrlById(req, res) {
+  const { urlId } = req.params;
+
+  if (!urlId || typeof urlId !== "number") {
+    return res.status(401).send({ error: "Url sent as params is not valid" });
+  }
+
+  try {
+    const query = await connection.query(
+      `SELECT "id", "shortUrl", "fullUrl" AS url FROM urls WHERE id = $1;`,
+      [urlId]
+    );
+
+    if (query.rowCount === 0) {
+      return res.status(404).send({ error: "Url does not exist" });
+    }
+
+    const url = {
+      id: query.rows[0].id,
+      shortUrl: query.rows[0].shortUrl,
+      url: query.rows[0].url,
+    };
+    return res.status(200).send(url);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
+export { shortUrl, getUrlById };
--- src/routes/urls.router.js
+++ src/routes/urls.router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { shortUrl } from "../controllers/urls.controllers.js";
+import { shortUrl, getUrlById } from "../controllers/urls.controllers.js";
 import { validateUrl } from "../middlewares/urls.middlewares.js";
 import { validateToken } from "../middlewares/token.middlewares.js";
 
 const router = express.Router();
 
 router.post("/shorten", validateToken, validateUrl, shortUrl);
+router.get("/:urlId", getUrlById);
 
 export default router;
