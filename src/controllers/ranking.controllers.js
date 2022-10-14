9ce497adc8c5a90cbf87e67e05bbab68120bb46e
gacdutra <ga.acdutra@gmail.com>
2022-10-14 10:12:53 -0300

    feat: add GET/ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -2,14 +2,16 @@ import connection from "../database/database.js";
 
 async function listRanking(req, res) {
   try {
-    const urlList =
-      await connection.query(`SELECT urls.id AS "urlId", urls."shortUrl", urls."fullUrl" as url, COUNT(*) AS "visitCount" 
-    FROM visits 
-    JOIN urls ON urls.id = visits."urlId"
-    GROUP BY urls.id
-    ORDER BY COUNT(*) DESC
-    LIMIT 10;`);
-    return res.send(urlList.rows);
+    const rankingByUser =
+      await connection.query(`SELECT links.id, links.name, links."linksCount", COUNT(urls."userId") AS "visitCount" FROM urls
+      JOIN visits ON visits."urlId" = urls.id
+      RIGHT JOIN (SELECT users.id, users.name, count(urls."userId") as "linksCount" from users 
+      LEFT JOIN urls on users.id = urls."userId"
+      GROUP BY urls."userId", users.id, users.name) AS links ON links.id = urls."userId"
+      GROUP BY links.id, links.name, links."linksCount"
+      ORDER BY "visitCount" DESC
+      LIMIT 10;`);
+    return res.status(200).send(rankingByUser.rows);
   } catch (error) {
     return res.status(500).send(error.message);
   }
